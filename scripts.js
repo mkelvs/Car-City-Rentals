dc516e6088898343dff45db544eaecd9329110d9
Mark Libario <mkelvin.libario@gmail.com>
2019-08-26 11:51:49 -0400

    added fleet.html
    added fadein effects

--- scripts.js
+++ scripts.js
@@ -17,8 +17,4 @@ var swiper = new Swiper('.swiper-container', {
     });
 
 $(".heading").hide().fadeIn(2000);
-$(".btn-1").click(function() {
-  $(".heading").slideUp(1000);
-  $("#main-page").fadeOut(100).fadeIn(500).removeClass("bg-img").addClass("bg-fleet");
-  $(".swiper-container").removeClass("hidden");
-})
+$("#swiper").hide().fadeIn(2000);
